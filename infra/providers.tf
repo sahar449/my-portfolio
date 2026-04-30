49848c15b05370de76bd7e7ea10b1e8b3702c230
Sahar <saharr449@gmail.com>
2026-04-30 13:13:09 +0300

    wip

--- infra/providers.tf
+++ infra/providers.tf
@@ -2,24 +2,6 @@ provider "aws" {
   region = var.region
 }
 
-data "aws_eks_cluster" "cluster" {
-  name       = var.cluster_name
-  depends_on = [module.eks]
-}
-
-data "aws_eks_cluster_auth" "cluster" {
-  name       = var.cluster_name
-  depends_on = [module.eks]
-}
-
-provider "helm" {
-  kubernetes {
-    host                   = data.aws_eks_cluster.cluster.endpoint
-    cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority[0].data)
-    token                  = data.aws_eks_cluster_auth.cluster.token
-  }
-}
-
 terraform {
   required_providers {
     aws = {
@@ -29,10 +11,6 @@ terraform {
     random = {
       source  = "hashicorp/random"
       version = "~> 3.5"
-    }
-    helm = {
-      source  = "hashicorp/helm"
-      version = "~> 2.13"
     }
   }
 
