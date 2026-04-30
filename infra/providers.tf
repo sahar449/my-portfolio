928070aef8f457336668bd8dd084ad0832f96856
Sahar <saharr449@gmail.com>
2026-04-30 13:00:13 +0300

    wip

--- infra/providers.tf
+++ infra/providers.tf
@@ -3,11 +3,13 @@ provider "aws" {
 }
 
 data "aws_eks_cluster" "cluster" {
-  name = var.cluster_name
+  name       = var.cluster_name
+  depends_on = [module.eks]
 }
 
 data "aws_eks_cluster_auth" "cluster" {
-  name = var.cluster_name
+  name       = var.cluster_name
+  depends_on = [module.eks]
 }
 
 provider "helm" {
